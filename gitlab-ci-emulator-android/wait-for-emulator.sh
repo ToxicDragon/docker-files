58ec5d3a1013be7b6706f0439d29faa7373635e4
Christian Ohrfandl <christian.ohrfandl@gmail.com>
2019-10-04 16:20:39 +0200

    added emualor wait

---
+++ gitlab-ci-emulator-android/wait-for-emulator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+EMU_BOOTED="no"
+while [[ ${EMU_BOOTED} != *"stopped"* ]]; do
+    echo "Wating for emulator booted..."
+    sleep 5
+    EMU_BOOTED=$(adb shell getprop init.svc.bootanim || echo "no")
+done
+
+echo "Emulator booted"
